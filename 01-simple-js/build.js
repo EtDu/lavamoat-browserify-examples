dc909f9aca803f47c4416a86fa5b16ad064d1a20
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-02-06 13:33:36 +0800

    Added Budo live reload option, Bonus: appends config override to index to include it in the source tree

--- 01-simple-js/build.js
+++ 01-simple-js/build.js
@@ -1,25 +1,57 @@
 const fs = require('fs')
 const browserify = require('browserify')
+const budo = require('budo')
+const lavamoat = require('lavamoat-browserify')
 
 // Autoconfig option
 let autoConfigEnabled = process.env.AUTOCONFIG
 // Initialize LavaMoat configuration object
 let lavamoatOpts = {}
 
+// Configure Browserify
 // Enable config autogen if specified, otherwise set the config to the default path
+
 if (autoConfigEnabled) {
   lavamoatOpts.writeAutoConfig = true
+  bundle()
+
 } else {
   lavamoatOpts.config = './lavamoat/lavamoat-config.json'
+  
+  // If 'watch' option is passed through, enable hot reloading when the override config file changes 
+  if (2 in process.argv) {
+    budo('./index.js', {
+      live: true,
+      stream: process.stdout,
+      port: 8000,
+      browserify: {
+        plugin: [
+          [lavamoat, lavamoatOpts]
+        ]
+      }
+    })
+  } else {
+    bundle()
+  }
 }
 
-// Configure Browserify
-const bundler = browserify(['./index.js'], {
-  plugin: [
-    ['lavamoat-browserify', lavamoatOpts]
-  ]
-})
+//Helper bundle function
+async function bundle() {
+  const requireOverrideConfig = "require('./lavamoat/lavamoat-config-override.json')"
+  const bundler = browserify(['./index.js'], {
+    plugin: [
+      ['lavamoat-browserify', lavamoatOpts]
+    ]
+  })
 
-// If running without writeAutoConfig, bundle and write to disk
-bundler.bundle()
+  bundler.bundle()
+  .on('end', () => {
+    const indexFile = fs.readFileSync('./index.js', { encoding: 'utf-8' })
+    if (!indexFile.includes(requireOverrideConfig)) {
+      fs.appendFileSync('./index.js', "\n\n" + requireOverrideConfig)
+    }
+  })
   .pipe(fs.createWriteStream('./bundle.js'))
+}
+
+
